c8d0dc227a0c5972846f3c6632313dcba22a03cf
Nico Dee <nicolas.dee@whu.edu>
2018-11-12 10:49:23 +0100

    added snazzy maps

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -1,37 +1,136 @@
 import GMaps from 'gmaps/gmaps.js';
+const styles = [
+{
+  "featureType": "administrative",
+  "elementType": "all",
+  "stylers": [
+  {
+    "visibility": "on"
+  },
+  {
+    "lightness": 33
+  }
+  ]
+},
+{
+  "featureType": "landscape",
+  "elementType": "all",
+  "stylers": [
+  {
+    "color": "#f2e5d4"
+  }
+  ]
+},
+{
+  "featureType": "poi.park",
+  "elementType": "geometry",
+  "stylers": [
+  {
+    "color": "#c5dac6"
+  }
+  ]
+},
+{
+  "featureType": "poi.park",
+  "elementType": "labels",
+  "stylers": [
+  {
+    "visibility": "on"
+  },
+  {
+    "lightness": 20
+  }
+  ]
+},
+{
+  "featureType": "road",
+  "elementType": "all",
+  "stylers": [
+  {
+    "lightness": 20
+  }
+  ]
+},
+{
+  "featureType": "road.highway",
+  "elementType": "geometry",
+  "stylers": [
+  {
+    "color": "#c5c6c6"
+  }
+  ]
+},
+{
+  "featureType": "road.arterial",
+  "elementType": "geometry",
+  "stylers": [
+  {
+    "color": "#e4d7c6"
+  }
+  ]
+},
+{
+  "featureType": "road.local",
+  "elementType": "geometry",
+  "stylers": [
+  {
+    "color": "#fbfaf7"
+  }
+  ]
+},
+{
+  "featureType": "water",
+  "elementType": "all",
+  "stylers": [
+  {
+    "visibility": "on"
+  },
+  {
+    "color": "#acbcc9"
+  }
+  ]
+}
+];
+
 
 const mapElement = document.getElementById('map');
 if (mapElement) { // don't try to build a map if there's no div#map to inject in
   const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
-   const markersDestination = JSON.parse(mapElement.dataset.destination);
-   const markersHometown = JSON.parse(mapElement.dataset.hometown);
+const markersDestination = JSON.parse(mapElement.dataset.destination);
+const markersHometown = JSON.parse(mapElement.dataset.hometown);
 
 
 
-  console.log(mapElement.dataset);
+console.log(mapElement.dataset);
 
-  map.addMarkers(markersDestination);
-  map.addMarkers(markersHometown);
+map.addMarkers(markersDestination);
+map.addMarkers(markersHometown);
 
 
-   if (markersDestination.length === 0) {
-    map.setZoom(2);
-  } else if (markersDestination.length === 1) {
-    map.setCenter(markersDestination[0].lat, markersDestination[0].lng);
-    map.setZoom(14);
-  } else {
-    map.fitLatLngBounds(markersDestination);
-  }
-
-  if (markersHometown.length === 0) {
-    map.setZoom(2);
-  } else if (markersHometown.length === 1) {
-    map.setCenter(markersHometown[0].lat, markersHometown[0].lng);
-    map.setZoom(14);
-  } else {
-    map.fitLatLngBounds(markersHometown);
-  }
+if (markersDestination.length === 0) {
+  map.setZoom(2);
+} else if (markersDestination.length === 1) {
+  map.setCenter(markersDestination[0].lat, markersDestination[0].lng);
+  map.setZoom(14);
+} else {
+  map.fitLatLngBounds(markersDestination);
 }
+
+if (markersHometown.length === 0) {
+  map.setZoom(2);
+} else if (markersHometown.length === 1) {
+  map.setCenter(markersHometown[0].lat, markersHometown[0].lng);
+  map.setZoom(14);
+} else {
+  map.fitLatLngBounds(markersHometown);
+}
+map.addStyle({
+  styles: styles,
+  mapTypeId: 'map_style'
+});
+map.setStyle('map_style');
+}
+
 
 
 
