e466ab5cbfe46705106d1f9a3521ccdeee96e709
Darlene Graterol <dcgb93@gmail.com>
2018-11-14 05:55:00 -0800

    changed the alert message to delete the trip

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,3 +7,13 @@ $(".js-open-wait").click(function() {
      }, 100);
    });
 
+
+
+$(window).load(function() {
+  $('.post-module').hover(function() {
+    $(this).find('.description').stop().animate({
+      height: "toggle",
+      opacity: "toggle"
+    }, 300);
+  });
+});
