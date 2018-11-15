9c10204c543c15beee58a471655584f493953c7b
Nico Dee <nicolas.dee@whu.edu>
2018-11-15 11:55:43 +0100

    datepicker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,19 +1,1 @@
 //= require rails-ujs
-//= require_tree .
-
-$(".js-open-wait").click(function() {
-     setTimeout(function() {
-        $(".wait").show();
-     }, 100);
-   });
-
-
-
-$(window).load(function() {
-  $('.post-module').hover(function() {
-    $(this).find('.description').stop().animate({
-      height: "toggle",
-      opacity: "toggle"
-    }, 300);
-  });
-});
